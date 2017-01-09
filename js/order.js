5945e3a358a23378fa96c7098ee1abfa6d09bb37
NiklasBlume <niklasblume210795@gmail.com>
2017-01-09 18:20:45 +0100

    able to click on progress bar

--- js/order.js
+++ js/order.js
@@ -1,3 +1,55 @@
+function auswahl_pressed() {
+	document.getElementById("auswahl").style.display = "inherit";
+	document.getElementById("option").style.display = "none";
+	document.getElementById("prufen").style.display = "none";
+	document.getElementById("bestatigen").style.display = "none";
+	document.getElementById("info").style.display = "none";
+	document.getElementById("last").style.display = "none";
+	document.getElementById("p_auswahl").style.color = "red";
+	document.getElementById("p_option").style.color = "white";
+	document.getElementById("p_prufen").style.color = "white";
+	document.getElementById("p_bestatigen").style.color = "white";
+}
+
+function option_pressed() {
+	document.getElementById("auswahl").style.display = "none";
+	document.getElementById("option").style.display = "inherit";
+	document.getElementById("prufen").style.display = "none";
+	document.getElementById("bestatigen").style.display = "none";
+	document.getElementById("info").style.display = "none";
+	document.getElementById("last").style.display = "none";
+	document.getElementById("p_auswahl").style.color = "white";
+	document.getElementById("p_option").style.color = "red";
+	document.getElementById("p_prufen").style.color = "white";
+	document.getElementById("p_bestatigen").style.color = "white";
+}
+
+function prufen_pressed() {
+	document.getElementById("auswahl").style.display = "none";
+	document.getElementById("option").style.display = "none";
+	document.getElementById("prufen").style.display = "inherit";
+	document.getElementById("bestatigen").style.display = "none";
+	document.getElementById("info").style.display = "none";
+	document.getElementById("last").style.display = "none";
+	document.getElementById("p_auswahl").style.color = "white";
+	document.getElementById("p_option").style.color = "white";
+	document.getElementById("p_prufen").style.color = "red";
+	document.getElementById("p_bestatigen").style.color = "white";
+}
+
+function bestatigen_pressed() {
+	document.getElementById("auswahl").style.display = "none";
+	document.getElementById("option").style.display = "none";
+	document.getElementById("prufen").style.display = "none";
+	document.getElementById("bestatigen").style.display = "inherit";
+	document.getElementById("info").style.display = "none";
+	document.getElementById("last").style.display = "none";
+	document.getElementById("p_auswahl").style.color = "white";
+	document.getElementById("p_option").style.color = "white";
+	document.getElementById("p_prufen").style.color = "white";
+	document.getElementById("p_bestatigen").style.color = "red";
+}
+
 function auswahl_weiter() {
 	document.getElementById("option").style.display = "inherit";
 	document.getElementById("auswahl").style.display = "none";
@@ -31,4 +83,10 @@ function prufen_weiter() {
 	document.getElementById("prufen").style.display = "none";
 	document.getElementById("p_bestatigen").style.color = "red";
 	document.getElementById("p_prufen").style.color = "white";
+}
+
+function bestatigen_weiter() {
+	document.getElementById("last").style.display = "inherit";
+	document.getElementById("bestatigen").style.display = "none";
+	document.getElementById("p_bestatigen").style.color = "white";
 }
