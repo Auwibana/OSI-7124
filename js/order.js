4cbeac4da67d13735e1b33e25b3c4ca56f93b5a2
NiklasBlume <niklasblume210795@gmail.com>
2017-01-09 17:41:26 +0100

    javascript for buttons

--- js/order.js
+++ js/order.js
@@ -1,3 +1,34 @@
-function test() {
-	document.getElementById("test").innerHTML = "Hello Javascript!";
+function auswahl_weiter() {
+	document.getElementById("option").style.display = "inherit";
+	document.getElementById("auswahl").style.display = "none";
+	document.getElementById("p_option").style.color = "red";
+	document.getElementById("p_auswahl").style.color = "white";
+}
+
+function option_zuruck() {
+	document.getElementById("auswahl").style.display = "inherit";
+	document.getElementById("option").style.display = "none";
+	document.getElementById("p_auswahl").style.color = "red";
+	document.getElementById("p_option").style.color = "white";
+}
+
+function option_weiter() {
+	document.getElementById("prufen").style.display = "inherit";
+	document.getElementById("option").style.display = "none";
+	document.getElementById("p_prufen").style.color = "red";
+	document.getElementById("p_option").style.color = "white";
+}
+
+function prufen_zuruck() {
+	document.getElementById("option").style.display = "inherit";
+	document.getElementById("prufen").style.display = "none";
+	document.getElementById("p_option").style.color = "red";
+	document.getElementById("p_prufen").style.color = "white";
+}
+
+function prufen_weiter() {
+	document.getElementById("bestatigen").style.display = "inherit";
+	document.getElementById("prufen").style.display = "none";
+	document.getElementById("p_bestatigen").style.color = "red";
+	document.getElementById("p_prufen").style.color = "white";
 }
