379b867a9ea69f9d3d743fbf05493cce7c764693
NiklasBlume <niklasblume210795@gmail.com>
2017-01-23 17:22:09 +0100

    something

--- js/index.js
+++ js/index.js
@@ -1,122 +1,132 @@
-
 // Diese Funktion kann zum suchen und Autovervollständigen von Positionseingaben des Nutzers verwendet werden kann.
 // soll hier nicht verwendet werden sondern gilt als Template für die Texteingabe von Adressen
 function initService() {
-  var displaySuggestions = function(predictions, status) {
-    // Überprüft nur den Status der Seite
-    if (status != google.maps.places.PlacesServiceStatus.OK) {
-      alert(status);
-      return;
-    }
-    // Zeigt alle Orte zur Autovervollständigung an
-    predictions.forEach(function(prediction) {
-      var li = document.createElement('li');
-      li.appendChild(document.createTextNode(prediction.description));
-      document.getElementById('results').appendChild(li);
+    var displaySuggestions = function(predictions, status) {
+        // Überprüft nur den Status der Seite
+        if (status != google.maps.places.PlacesServiceStatus.OK) {
+            alert(status);
+            return;
+        }
+        // Zeigt alle Orte zur Autovervollständigung an
+        predictions.forEach(function(prediction) {
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(prediction.description));
+            document.getElementById('results').appendChild(li);
+        });
+    };
+
+    var service = new google.maps.places.AutocompleteService();
+
+    // ich glaube das braucht man nicht
+    //service.getQueryPredictions({ input: 'pizza near Syd' }, displaySuggestions);
+
+    // hier muss das input field vom type text stehen
+    var start = document.getElementById('start');
+    var ziel = document.getElementById('ziel');
+    var zwischenstops = document.getElementById('zwischenstops');
+
+    // das ist das SearchBox Objekt das die Suggestions handled
+    var searchStart = new google.maps.places.SearchBox(start, {
+        //bounds: defaultBounds
+
+        // das hier beschränkt die suche auf Deutschland
+        // habe aber nicht das gefühl das die restriction wirklich functioniert
+        componentRestrictions: {
+            country: 'de'
+        }
     });
-  };
 
-  var service = new google.maps.places.AutocompleteService();
+    var searchZiel = new google.maps.places.SearchBox(ziel, {
+        //bounds: defaultBounds
 
-  // ich glaube das braucht man nicht
-  //service.getQueryPredictions({ input: 'pizza near Syd' }, displaySuggestions);
+        // das hier beschränkt die suche auf Deutschland
+        // habe aber nicht das gefühl das die restriction wirklich functioniert
+        componentRestrictions: {
+            country: 'de'
+        }
+    });
 
-  // hier muss das input field vom type text stehen
-  var start = document.getElementById('start');
-  var ziel = document.getElementById('ziel');
-  var zwischenstops = document.getElementById('zwischenstops');
+    var searchZwischenstops = new google.maps.places.SearchBox(zwischenstops, {
+        //bounds: defaultBounds
 
-  // das ist das SearchBox Objekt das die Suggestions handled
-  var searchStart = new google.maps.places.SearchBox(start, {
-    //bounds: defaultBounds
+        // das hier beschränkt die suche auf Deutschland
+        // habe aber nicht das gefühl das die restriction wirklich functioniert
+        componentRestrictions: {
+            country: 'de'
+        }
+    });
 
-    // das hier beschränkt die suche auf Deutschland
-    // habe aber nicht das gefühl das die restriction wirklich functioniert
-    componentRestrictions: {country: 'de'}
-  });
+    // hier mit kann man die Suche auf die Umgebung des Nutzers beschränken
+    // Hier sollte man noch die Dynamamische Position des Users einbauen
+    // kann wahrscheinlich aus kommentiert werden
+    // oben die suche auf deutschland beschränkt
+    searchStart.addListener('places_changed', function() {
+        var places = searchStart.getPlaces();
 
-  var searchZiel = new google.maps.places.SearchBox(ziel, {
-    //bounds: defaultBounds
+        if (places.length == 0) {
+            return;
+        }
+        // Sobald sich der Nutzer für eine Adresse entschieden hat das ergbeniss von hier ausgelesen werden.
+        //alert(searchStart.getPlaces()[0].formatted_address);
+    });
 
-    // das hier beschränkt die suche auf Deutschland
-    // habe aber nicht das gefühl das die restriction wirklich functioniert
-    componentRestrictions: {country: 'de'}
-  });
+    searchZiel.addListener('places_changed', function() {
+        var places = searchZiel.getPlaces();
 
-  var searchZwischenstops = new google.maps.places.SearchBox(zwischenstops, {
-    //bounds: defaultBounds
+        if (places.length == 0) {
+            return;
+        }
+        // Sobald sich der Nutzer für eine Adresse entschieden hat das ergbeniss von hier ausgelesen werden.
+        //alert(searchZiel.getPlaces()[0].formatted_address);
+    });
 
-    // das hier beschränkt die suche auf Deutschland
-    // habe aber nicht das gefühl das die restriction wirklich functioniert
-    componentRestrictions: {country: 'de'}
-  });
+    searchZwischenstops.addListener('places_changed', function() {
+        var places = searchZwischenstops.getPlaces();
 
-  // hier mit kann man die Suche auf die Umgebung des Nutzers beschränken
-  // Hier sollte man noch die Dynamamische Position des Users einbauen
-  // kann wahrscheinlich aus kommentiert werden
-  // oben die suche auf deutschland beschränkt
-  searchStart.addListener('places_changed', function() {
-    var places = searchStart.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-    // Sobald sich der Nutzer für eine Adresse entschieden hat das ergbeniss von hier ausgelesen werden.
-    //alert(searchStart.getPlaces()[0].formatted_address);
-  });
-
-  searchZiel.addListener('places_changed', function() {
-    var places = searchZiel.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-    // Sobald sich der Nutzer für eine Adresse entschieden hat das ergbeniss von hier ausgelesen werden.
-    //alert(searchZiel.getPlaces()[0].formatted_address);
-  });
-
-  searchZwischenstops.addListener('places_changed', function() {
-    var places = searchZwischenstops.getPlaces();
-
-    if (places.length == 0) {
-      return;
-    }
-    // Sobald sich der Nutzer für eine Adresse entschieden hat das ergbeniss von hier ausgelesen werden.
-    //alert(searchZiel.getPlaces()[0].formatted_address);
-  });
+        if (places.length == 0) {
+            return;
+        }
+        // Sobald sich der Nutzer für eine Adresse entschieden hat das ergbeniss von hier ausgelesen werden.
+        //alert(searchZiel.getPlaces()[0].formatted_address);
+    });
 }
 
 // eine leichtere Function um die Geodaten auszulesen
 function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      geolocate2(geolocation.lat, geolocation.lng);
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            geolocate2(geolocation.lat, geolocation.lng);
 
-      // Diese Geodaten könnten dann als user position in die Datenbank reingemacht werden
-      // position.coords.latitude
-      // position.coords.longitude
-    });
-  }
+            // Diese Geodaten könnten dann als user position in die Datenbank reingemacht werden
+            // position.coords.latitude
+            // position.coords.longitude
+        });
+    }
 }
 
 
-function geolocate2(lat, lng){
-  var geocoder = new google.maps.Geocoder;
-  var latlng = {lat: lat, lng: lng};
-  geocoder.geocode({'location': latlng}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      if(results[0]){
-        document.getElementById('start').value = results[0].formatted_address;
-      } else {
-        window.alert('Geocoder failed due to: ' + status);
-      }
-  } else {
-       window.alert('Geocoder failed due to: ' + status);
-     }
+function geolocate2(lat, lng) {
+    var geocoder = new google.maps.Geocoder;
+    var latlng = {
+        lat: lat,
+        lng: lng
+    };
+    geocoder.geocode({
+        'location': latlng
+    }, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+                document.getElementById('start').value = results[0].formatted_address;
+            } else {
+                window.alert('Geocoder failed due to: ' + status);
+            }
+        } else {
+            window.alert('Geocoder failed due to: ' + status);
+        }
 
-  });
+    });
 }
--- js/infoPage.js
+++ js/infoPage.js
@@ -6,30 +6,30 @@ var time
 var date
 
 requestGet.onreadystatechange = function() {
-	// console.log("onreadystatechange: " + requestGet.readyState + ", " +  requestGet.status);
-	// console.log(requestGet.responseText);
-	if (requestGet.readyState == 4) {
-		if (requestGet.status == 200) {
-			var response = JSON.parse(requestGet.responseText);
-			handlersGet[response._id](response);
-		}
-		if (requestGet.status == 404) {
-			console.log("not found: " + requestGet.responseText);
-		}
-	}
+    // console.log("onreadystatechange: " + requestGet.readyState + ", " +  requestGet.status);
+    // console.log(requestGet.responseText);
+    if (requestGet.readyState == 4) {
+        if (requestGet.status == 200) {
+            var response = JSON.parse(requestGet.responseText);
+            handlersGet[response._id](response);
+        }
+        if (requestGet.status == 404) {
+            console.log("not found: " + requestGet.responseText);
+        }
+    }
 };
 
 function get(variable) {
-	// console.log("get " + variable);
-	requestGet.open("GET", dburl + variable, false);
-	requestGet.send();
+    // console.log("get " + variable);
+    requestGet.open("GET", dburl + variable, false);
+    requestGet.send();
 }
 
 function update() {
-	for (var name in handlersGet) {
-		// console.log("updating " + name);
-		get(name);
-	}
+    for (var name in handlersGet) {
+        // console.log("updating " + name);
+        get(name);
+    }
 }
 
 
@@ -39,156 +39,159 @@ function update() {
 var dbname = "hci1";
 var dburl = "http://127.0.0.1:5984/" + dbname + "/";
 var handlersGet = {
-	"option" : optionGet,
-	"auswahl": auswahlGet
+    "option": optionGet,
+    "auswahl": auswahlGet
 };
 
 function auswahlGet(response) {
-	start = response.start
-	end = response.end
-	time = response.time
-	date = response.date
+    start = response.start
+    end = response.end
+    time = response.time
+    date = response.date
 }
 
 function optionGet(response) {
-	zwischen = response.zwischenstops
+    zwischen = response.zwischenstops
 }
 
-$(document).ready(function(){
-	document.getElementById('waitingtime').innerHTML = "Ankunft: "+date + " "+ time
+$(document).ready(function() {
+    document.getElementById('waitingtime').innerHTML = "Ankunft: " + date + " " + time
 })
 
 
 function myMap() {
 
-	var map;
-	directionsDisplay = new google.maps.DirectionsRenderer();
+    var map;
+    directionsDisplay = new google.maps.DirectionsRenderer();
 
 
-	var mapOptions = {
-		// change the center of the map to users location or hardcode it for a borad view on Germany
-		center: new google.maps.LatLng(52.3758916, 9.7320104),
-		zoom: 11
-	}
+    var mapOptions = {
+        // change the center of the map to users location or hardcode it for a borad view on Germany
+        center: new google.maps.LatLng(52.3758916, 9.7320104),
+        zoom: 11
+    }
 
-	map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-
-	directionsDisplay.setMap(map);
-
-	// Directions panel gibt eine Weg Beschreibung die brauchen wir aber gerade nicht.
-	//directionsDisplay.setPanel(document.getElementById("directionsPanel"));
-
-	showCar(map)
-
-	// calcRoute(directionsDisplay);
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 
-	// nachdem das Auto angekommen ist
-	// map.clearOverlays();
+    directionsDisplay.setMap(map);
 
-	// Dann veschwindet der Marker wieder.
-	// oder marker.setMap(null)
+    // Directions panel gibt eine Weg Beschreibung die brauchen wir aber gerade nicht.
+    //directionsDisplay.setPanel(document.getElementById("directionsPanel"));
+
+    showCar(map)
+
+    // calcRoute(directionsDisplay);
+
+
+    // nachdem das Auto angekommen ist
+    // map.clearOverlays();
+
+    // Dann veschwindet der Marker wieder.
+    // oder marker.setMap(null)
 }
 // CalcRoute sollte modifizierbarer sein, Das heißt mit Argumenten kann man eine beliebige Strecke planen.
 // Dann kann man diese Funktion auch benutzen um die Strecke des Autos zum Startpunkt zu planen.
 
 function calcRoute(directionsDisplay) {
 
-	var directionsService = new google.maps.DirectionsService();
+    var directionsService = new google.maps.DirectionsService();
 
-	// Es können beliebig viele waypoints hier erstellt werden, um sie als zwischenstops zu nutzen
-	get("option")
-	var  waypoints = [];
+    // Es können beliebig viele waypoints hier erstellt werden, um sie als zwischenstops zu nutzen
+    get("option")
+    var waypoints = [];
 
-	zwischen.forEach((n) => {
-		var waypoints_obj = {
-			location: n,
-			stopover: true
-		};
-		waypoints.push(waypoints_obj);
-	})
+    zwischen.forEach((n) => {
+        var waypoints_obj = {
+            location: n,
+            stopover: true
+        };
+        waypoints.push(waypoints_obj);
+    })
 
-	// Beispiele zur Eingabe der Zeit
+    // Beispiele zur Eingabe der Zeit
 
-	//var today = new Date();
-	//var birthday = new Date("December 17, 1995 03:24:00");
-	//var birthday = new Date("1995-12-17T03:24:00");
-	//var birthday = new Date(1995,11,17);
- 	//var birthday = new Date(1995,11,17,3,24,0);
+    //var today = new Date();
+    //var birthday = new Date("December 17, 1995 03:24:00");
+    //var birthday = new Date("1995-12-17T03:24:00");
+    //var birthday = new Date(1995,11,17);
+    //var birthday = new Date(1995,11,17,3,24,0);
 
-	var departure_time = new Date().getTime() / 1000;
+    var departure_time = new Date().getTime() / 1000;
 
-	get("auswahl")
-  var request = {
-    origin: start,
-    destination: end,
-    travelMode: google.maps.TravelMode.DRIVING,
-		waypoints: waypoints,
-		optimizeWaypoints: true,
-		departure_time: departure_time
-  };
-  directionsService.route(request, function(result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(result);
-    }
-  });
+    get("auswahl")
+    var request = {
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode.DRIVING,
+        waypoints: waypoints,
+        optimizeWaypoints: true,
+        departure_time: departure_time
+    };
+    directionsService.route(request, function(result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+        }
+    });
 }
 
-function showCar(map){
+function showCar(map) {
 
-	var carLattLng = {lat: 52.3759, lng: 9.732};
+    var carLattLng = {
+        lat: 52.3759,
+        lng: 9.732
+    };
 
-	var relativePixelSize = scaleImageWIthZoom(map);
+    var relativePixelSize = scaleImageWIthZoom(map);
 
-	var marker = new google.maps.Marker({
-		position: carLattLng,
-		map: map,
-		title: "Dein Auto",
-		icon: null
-	});
+    var marker = new google.maps.Marker({
+        position: carLattLng,
+        map: map,
+        title: "Dein Auto",
+        icon: null
+    });
 
-	setMarkerIcon(marker, relativePixelSize);
+    setMarkerIcon(marker, relativePixelSize);
 
-	addInfoWindowToMarker(map, marker, "Auto");
+    addInfoWindowToMarker(map, marker, "Auto");
 
-	google.maps.event.addListener(map, "zoom_changed", function(){
+    google.maps.event.addListener(map, "zoom_changed", function() {
 
-		var relativePixelSize = scaleImageWIthZoom(map);
-		setMarkerIcon(marker, relativePixelSize);
+        var relativePixelSize = scaleImageWIthZoom(map);
+        setMarkerIcon(marker, relativePixelSize);
 
-	});
+    });
 }
 
-function scaleImageWIthZoom(map){
-	var pixelSizeAtZoom0 = 8; //the size of the icon at zoom level 0
-	var maxPixelSize = 350; //restricts the maximum size of the icon, otherwise the browser will choke at higher zoom levels trying to scale an image to millions of pixels
+function scaleImageWIthZoom(map) {
+    var pixelSizeAtZoom0 = 8; //the size of the icon at zoom level 0
+    var maxPixelSize = 350; //restricts the maximum size of the icon, otherwise the browser will choke at higher zoom levels trying to scale an image to millions of pixels
 
-	var zoom = map.getZoom();
-	var relativePixelSize = Math.round(pixelSizeAtZoom0*Math.pow(2,zoom)); // use 2 to the power of current zoom to calculate relative pixel size.  Base of exponent is 2 because relative size should double every time you zoom in
+    var zoom = map.getZoom();
+    var relativePixelSize = Math.round(pixelSizeAtZoom0 * Math.pow(2, zoom)); // use 2 to the power of current zoom to calculate relative pixel size.  Base of exponent is 2 because relative size should double every time you zoom in
 
-	if(relativePixelSize > maxPixelSize) //restrict the maximum size of the icon
-		relativePixelSize = maxPixelSize;
+    if (relativePixelSize > maxPixelSize) //restrict the maximum size of the icon
+        relativePixelSize = maxPixelSize;
 
-	return relativePixelSize / 16; //  16 is a costum modifier because this code is written for images with the size 8x8, momentarly the image is 256x256
+    return relativePixelSize / 16; //  16 is a costum modifier because this code is written for images with the size 8x8, momentarly the image is 256x256
 }
 
-function setMarkerIcon(marker, relativePixelSize){
-	marker.setIcon(
-		new google.maps.MarkerImage(
-		'http://icons.iconarchive.com/icons/graphicloads/colorful-long-shadow/128/Car-icon.png',
-		null,
-		new google.maps.Point(0, 0), // origin
-		new google.maps.Point(relativePixelSize / 2, relativePixelSize / 2), // anchor
-		new google.maps.Size(relativePixelSize, relativePixelSize	)
-		)
-	)
+function setMarkerIcon(marker, relativePixelSize) {
+    marker.setIcon(
+        new google.maps.MarkerImage(
+            'http://icons.iconarchive.com/icons/graphicloads/colorful-long-shadow/128/Car-icon.png',
+            null,
+            new google.maps.Point(0, 0), // origin
+            new google.maps.Point(relativePixelSize / 2, relativePixelSize / 2), // anchor
+            new google.maps.Size(relativePixelSize, relativePixelSize)
+        )
+    )
 }
 
-function addInfoWindowToMarker(map, marker, contentString){
-	var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
+function addInfoWindowToMarker(map, marker, contentString) {
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
 
-	infowindow.open(map, marker)
+    infowindow.open(map, marker)
 }
